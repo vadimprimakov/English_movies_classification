32bd69305074aa11788e536eb9ea309441177246
vadimprimakov <vad.prim@yandex.ru>
2023-04-29 12:37:20 +0300

    level bar

--- app/app.py
+++ app/app.py
@@ -102,7 +102,7 @@ if upload_file:
     level_bar = st.progress(0)
     for i in range(round(1/6 * make_level_bar(df[features], model) * 100)):
         level_bar.progress(i)
-        time.sleep(0.001)
+       # time.sleep(0.001)
         
         button = st.button('Показать анализ')
         if button:
